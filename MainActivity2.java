7bae277e887e8b90e31c32a5605a3b432a8146ea
Tearfall <108473064+Tearfall@users.noreply.github.com>
2023-05-14 08:07:54 +0800

    Add files via upload

---
+++ MainActivity2.java
@@ -0,0 +1,24 @@
+package com.jar.swapperandchecker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity2 extends AppCompatActivity {
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        textView = (TextView) findViewById(R.id.result);
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        if(extras != null){
+            textView.setText(extras.getString("result"));
+        }
+    }
+}
